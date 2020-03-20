cc0b0af8c82e577f1c900f2f5a848e4e8e9a4de2
rvignav <rvignav@gmail.com>
2020-03-19 22:50:05 -0700

    fixed contact form

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// import firebase from 'firebase/app';
-// import 'firebase/database';
 var config = {
   apiKey: "AIzaSyClTM0ddPhZgPoVtifd8TZ0c-LrC0iZshs",
   authDomain: "covid19saratoga.firebaseapp.com",
@@ -10,173 +8,31 @@ var config = {
   appId: "1:1092626000161:web:86ad46738af11ef21cb660",
   measurementId: "G-ZFG19C7YDN"
 };
-  
-  // Initialize your Firebase app
-  var app = firebase.initializeApp(config);
-  
-  // Reference to the recommendations object in your Firebase database
-  var ref = firebase.database().ref("markers");
-  
-  // Save a new recommendation to the database, using the input in the form
-  var submit = function () {
-  
-    // Get input values from each of the form elements
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var address = $("#address").val();
-    var subject = $("#subject").val();
-    var message = $("#message").val();
-  
-    // Push a new recommendation to the database using those values
-    ref.push({
-      "name": name,
-      "email": email,
-      "address": address,
-      "subject": subject,
-      "message": message
-    }).then(function(ref) {
-      console.log(ref.parent + "/" + ref.key);
-    })
-    .catch(function(error) {
-      console.log(error);
-    })
-  };
-  
-  // When the window is fully loaded, call this function.
-  // Note: because we are attaching an event listener to a particular HTML element
-  // in this function, we can't do that until the HTML element in question has
-  // been loaded. Otherwise, we're attaching our listener to nothing, and no code
-  // will run when the submit button is clicked.
-  // $(window).load(function () {
-  
-    // Find the HTML element with the id recommendationForm, and when the submit
-    // event is triggered on that element, call submitRecommendation.
-  $("#formbutton").click(function() {
-    console.log('IN SUBMIT');
-    // $('#markerForm').validate()
-    submit();
-    location.reload();
-  });
-  
-  // });
-  
-  
-  // jQuery(document).ready(function($) {
-  //   "use strict";
-  
-  //   //Contact
-  //   $('form.contactForm').submit(function() {
-  //     var f = $(this).find('.form-group'),
-  //       ferror = false,
-  //       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
-  
-  //     f.children('input').each(function() { // run all inputs
-  
-  //       var i = $(this); // current input
-  //       var rule = i.attr('data-rule');
-  
-  //       if (rule !== undefined) {
-  //         var ierror = false; // error flag for current input
-  //         var pos = rule.indexOf(':', 0);
-  //         if (pos >= 0) {
-  //           var exp = rule.substr(pos + 1, rule.length);
-  //           rule = rule.substr(0, pos);
-  //         } else {
-  //           rule = rule.substr(pos + 1, rule.length);
-  //         }
-  
-  //         switch (rule) {
-  //           case 'required':
-  //             if (i.val() === '') {
-  //               ferror = ierror = true;
-  //             }
-  //             break;
-  
-  //           case 'minlen':
-  //             if (i.val().length < parseInt(exp)) {
-  //               ferror = ierror = true;
-  //             }
-  //             break;
-  
-  //           case 'email':
-  //             if (!emailExp.test(i.val())) {
-  //               ferror = ierror = true;
-  //             }
-  //             break;
-  
-  //           case 'checked':
-  //             if (! i.is(':checked')) {
-  //               ferror = ierror = true;
-  //             }
-  //             break;
-  
-  //           case 'regexp':
-  //             exp = new RegExp(exp);
-  //             if (!exp.test(i.val())) {
-  //               ferror = ierror = true;
-  //             }
-  //             break;
-  //         }
-  //         i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-  //       }
-  //     });
-  //     f.children('textarea').each(function() { // run all inputs
-  
-  //       var i = $(this); // current input
-  //       var rule = i.attr('data-rule');
-  
-  //       if (rule !== undefined) {
-  //         var ierror = false; // error flag for current input
-  //         var pos = rule.indexOf(':', 0);
-  //         if (pos >= 0) {
-  //           var exp = rule.substr(pos + 1, rule.length);
-  //           rule = rule.substr(0, pos);
-  //         } else {
-  //           rule = rule.substr(pos + 1, rule.length);
-  //         }
-  
-  //         switch (rule) {
-  //           case 'required':
-  //             if (i.val() === '') {
-  //               ferror = ierror = true;
-  //             }
-  //             break;
-  
-  //           case 'minlen':
-  //             if (i.val().length < parseInt(exp)) {
-  //               ferror = ierror = true;
-  //             }
-  //             break;
-  //         }
-  //         i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-  //       }
-  //     });
-  //     if (ferror) return false;
-  //     else var str = $(this).serialize();
-  //     var action = $(this).attr('action');
-  //     if( ! action ) {
-  //       action = 'contactform/contactform.php';
-  //     }
-  //     $.ajax({
-  //       type: "POST",
-  //       url: action,
-  //       data: str,
-  //       success: function(msg) {
-  //         // alert(msg);
-  //         if (msg == 'OK') {
-  //           $("#sendmessage").addClass("show");
-  //           $("#errormessage").removeClass("show");
-  //           $('.contactForm').find("input, textarea").val("");
-  //         } else {
-  //           $("#sendmessage").removeClass("show");
-  //           $("#errormessage").addClass("show");
-  //           $('#errormessage').html(msg);
-  //         }
-  
-  //       }
-  //     });
-  //     return false;
-  //   });
-  
-  // });
-  
+var app = firebase.initializeApp(config);
+var ref = firebase.database().ref("markers");
+var submit = function () {
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var address = $("#address").val();
+  var subject = $("#subject").val();
+  var message = $("#message").val();
+
+  ref.push({
+    "name": name,
+    "email": email,
+    "address": address,
+    "subject": subject,
+    "message": message
+  }).then(function (ref) {
+    console.log(ref.parent + "/" + ref.key);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+};
+
+$("#formbutton").click(function () {
+  console.log('IN SUBMIT');
+  submit();
+  location.reload();
+});
