dc340ed7e52d9d0b3ffd4d2d2ec64a77cd9c0f0e
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-03-22 14:20:34 -0700

    Update .gitignores and protect the sendgrid API key

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,14 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
