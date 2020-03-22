290534fb4b1ba9ee4be589ca909d9deda9479e59
rvignav <rvignav@gmail.com>
2020-03-22 00:44:25 -0700

    fixed resize js file

--- js/resize-map-instructions.js
+++ js/resize-map-instructions.js
@@ -1,11 +1,19 @@
 $(window).resize(() => {
   if ($(window).width() <= 667) {
-    $('.map-instructions').html(`Click on a marker to view the request for help. You can also offer help 
-    by posting on the discussion thread that pops up. Double click a marker to permanently remove it from 
-    the map.`);
+    $('.map-instructions').html(`<b><em>Need something? </em></b> Fill out the form under the
+    map to add a marker.<br><br>
+    <b><em>Want to help?</em></b> Click on a marker to view the request for help. You <br>can offer help by
+    logging into Disqus and posting on the<br>discussion thread that pops up.
+    <br><br><b><em>Want to delete a marker?</em></b> Double click a marker to permanently<br>remove it from
+    the
+    map.`);
   } else {
-    $('.map-instructions').html(`Click on a marker to view the request for help. You can also offer help<br>by
-    posting on the discussion thread that pops up. Double click a<br>marker to permanently remove it from the
+    $('.map-instructions').html(`<b><em>Need something? </em></b> Fill out the form under the
+    map to add a marker.<br><br>
+    <b><em>Want to help?</em></b> Click on a marker to view the request for help. You <br>can offer help by
+    logging into Disqus and posting on the<br>discussion thread that pops up.
+    <br><br><b><em>Want to delete a marker?</em></b> Double click a marker to permanently<br>remove it from
+    the
     map.`);
   }
 });
