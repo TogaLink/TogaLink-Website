e0404be0b739518a15dc7cc685e1a07fe9c596b1
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-03-22 17:01:53 -0700

    Add automatic emailing (#17)

---
+++ functions/index.js
@@ -0,0 +1,33 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+let config = require('./env.json');
+if (Object.keys(functions.config()).length) {
+  config = functions.config();
+}
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(config.sendgrid.key);
+
+exports.onMarkerCreate = functions.database.ref('/markers/{postId}').onCreate((snapshot, _) => {
+  const { name, email, address, subject, message } = snapshot.val();
+  const msg = {
+    to: 'togalink2020@gmail.com', // TODO: change this to campaign@rishi2020.com
+    from: 'info@togalink.org',
+    replyTo: email,
+    subject: `[TogaLink] COVIDcare: ${name} needs your help!`,
+    text: 'Please view in html',
+    html: `${name} (${email}) has placed a marker at ${address}.
+    <br><br>
+    <strong>Subject</strong>: ${subject}
+    <br>
+    <strong>Message</strong>: ${message}`,
+  };
+  return sgMail.send(msg);
+});
