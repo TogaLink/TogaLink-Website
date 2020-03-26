c585b8e3f32507f5ff6a477f0dab217f9b65d878
rvignav <rvignav@gmail.com>
2020-03-26 12:25:36 -0700

    requests fix 1

--- js/map.js
+++ js/map.js
@@ -1,133 +1,133 @@
 let prevClickedMarker = null;
 
 function initMap() {
-  var mapOptions = {
-    zoom: 11,
-    center: new google.maps.LatLng(37.3875, -122.0575),
-    mapTypeId: 'roadmap'
-  };
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(37.3875, -122.0575),
+        mapTypeId: 'roadmap'
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  refMarkers.once("value", async function (snapshot) {
-    if (!snapshot.exists()) {
-      return;
-    }
-    var val = snapshot.val();
-    console.log(val);
-    var keys = Object.keys(val);
-    for (var i = 0; i < keys.length; i++) {
-      const k = keys[i];
-      if (k.startsWith('geo')) {
-        continue;
-      }
-      var address = val[k].address;
-      var titleFirebase = val[k].subject;
-      console.log(address, k);
-      while (address.indexOf(" ") != -1) {
-        address = address.replace(" ", "+");
-      }
-      const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-        params: {
-          address,
-          key: 'AIzaSyCUmA1jvhKOYygqrQMVJi8IJmXuW496HGk'
+    refMarkers.once("value", async function(snapshot) {
+        if (!snapshot.exists()) {
+            return;
         }
-      })
-      const marker = new google.maps.Marker({
-        position: response.data.results[0].geometry.location,
-        map,
-      });
-      marker.addListener('click', () => {
-        // TODO: Make all blue markers within a half-mile radius twice as large
-        const markerRef = k;
-        if (marker === prevClickedMarker) {
-          $('#discussion').toggle('slow');
-          // TODO: Make all blue markers normal size again
-        } else {
-          $('#discussion').show();
-          const { name, subject, message } = val[markerRef];
-          $('.assistance-request-title')
-            .text(`${name} needs your assistance`);
-          $('.assistance-request-subject')
-            .attr("value", subject);
-          $('.assistance-request-message')
-            .text(message);
-
-          DISQUS.reset({
-            reload: true,
-            config: function() {
-              this.page.url = `https://covid19saratoga.com/#!/${markerRef}`;
-              this.page.identifier = markerRef;
+        var val = snapshot.val();
+        console.log(val);
+        var keys = Object.keys(val);
+        for (var i = 0; i < keys.length; i++) {
+            const k = keys[i];
+            if (k.startsWith('geo')) {
+                continue;
             }
-          });
-        };
-        prevClickedMarker = marker;
-        setTimeout(function() {
-          location.hash = "discussionContainer";
-        }, 1000);
-      });
-      marker.addListener('dblclick', async function () {
-        if (confirm("Are you sure you want to delete this marker?")) {
-          console.log(k);
-          console.log(refMarkers.child(k));
-          await app.database().ref(`markers/${k}`).remove();
-          await app.database().ref(`markers/geo${k}`).remove();
-          if (marker === prevClickedMarker) {
-            $('#discussion').hide();
-            prevClickedMarker = null;
-          }
-          marker.setMap(null);
-        }
-      });
-    }
-  });
+            var address = val[k].address;
+            var titleFirebase = val[k].subject;
+            console.log(address, k);
+            while (address.indexOf(" ") != -1) {
+                address = address.replace(" ", "+");
+            }
+            const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address,
+                    key: 'AIzaSyCUmA1jvhKOYygqrQMVJi8IJmXuW496HGk'
+                }
+            })
+            const marker = new google.maps.Marker({
+                position: response.data.results[0].geometry.location,
+                map,
+            });
+            marker.addListener('click', () => {
+                // TODO: Make all blue markers within a half-mile radius twice as large
+                const markerRef = k;
+                if (marker === prevClickedMarker) {
+                    $('#discussion').toggle('slow');
+                    // TODO: Make all blue markers normal size again
+                } else {
+                    $('#discussion').show();
+                    const { name, subject, message } = val[markerRef];
+                    $('.assistance-request-title')
+                        .text(`${name} needs your assistance`);
+                    $('.assistance-request-subject')
+                        .attr("value", subject);
+                    $('.assistance-request-message')
+                        .text(message);
 
-  refVolunteers.once("value", async function (snapshot) {
-    if (!snapshot.exists()) {
-      return;
-    }
-    var val = snapshot.val();
-    console.log(val);
-    var keys = Object.keys(val);
-    for (var i = 0; i < keys.length; i++) {
-      const k = keys[i];
-      if (k.startsWith('geo')) {
-        continue;
-      }
-      var address = val[k].address;
-      var titleFirebase = val[k].subject;
-      console.log(address, k);
-      while (address.indexOf(" ") != -1) {
-        address = address.replace(" ", "+");
-      }
-      const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-        params: {
-          address,
-          key: 'AIzaSyCUmA1jvhKOYygqrQMVJi8IJmXuW496HGk'
+                    DISQUS.reset({
+                        reload: true,
+                        config: function() {
+                            this.page.url = `https://covid19saratoga.com/#!/${markerRef}`;
+                            this.page.identifier = markerRef;
+                        }
+                    });
+                };
+                prevClickedMarker = marker;
+                setTimeout(function() {
+                    location.hash = "discussionContainer";
+                }, 1000);
+            });
+            marker.addListener('dblclick', async function() {
+                if (confirm("Are you sure you want to delete this marker?")) {
+                    console.log(k);
+                    console.log(refMarkers.child(k));
+                    await app.database().ref(`markers/${k}`).remove();
+                    await app.database().ref(`markers/geo${k}`).remove();
+                    if (marker === prevClickedMarker) {
+                        $('#discussion').hide();
+                        prevClickedMarker = null;
+                    }
+                    marker.setMap(null);
+                }
+            });
         }
-      })
-      const marker = new google.maps.Marker({
-        position: response.data.results[0].geometry.location,
-        map,
-        icon: {                             
-          url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"                          
+    });
+
+    refVolunteers.once("value", async function(snapshot) {
+        if (!snapshot.exists()) {
+            return;
         }
-      });
-      marker.addListener('click', () => {
-        const markerRef = k;
-        const { name, email, phone } = val[markerRef];
-        var infowindow = new google.maps.InfoWindow({
-          content: "Name: " + name+", Email: " + email + ", Phone #: " + phone,
-        });
-        infowindow.open(map,marker);
-      });
-      // marker.addListener('dblclick', async function () {
-      //   if (confirm("Are you sure you want to delete this marker?")) {
-      //     console.log(k);
-      //     console.log(ref.child(k));
-      //     await app.database().ref(`volunteers/${k}`).remove();
-      //     marker.setMap(null);
-      //   }
-      // });
-    }
-  });
+        var val = snapshot.val();
+        console.log(val);
+        var keys = Object.keys(val);
+        for (var i = 0; i < keys.length; i++) {
+            const k = keys[i];
+            if (k.startsWith('geo')) {
+                continue;
+            }
+            var address = val[k].address;
+            var titleFirebase = val[k].subject;
+            console.log(address, k);
+            while (address.indexOf(" ") != -1) {
+                address = address.replace(" ", "+");
+            }
+            const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+                params: {
+                    address,
+                    key: 'AIzaSyCUmA1jvhKOYygqrQMVJi8IJmXuW496HGk'
+                }
+            })
+            const marker = new google.maps.Marker({
+                position: response.data.results[0].geometry.location,
+                map,
+                icon: {
+                    url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+                }
+            });
+            marker.addListener('click', () => {
+                const markerRef = k;
+                const { name, email, phone } = val[markerRef];
+                var infowindow = new google.maps.InfoWindow({
+                    content: "Name: " + name + ", Email: " + email,
+                });
+                infowindow.open(map, marker);
+            });
+            // marker.addListener('dblclick', async function () {
+            //   if (confirm("Are you sure you want to delete this marker?")) {
+            //     console.log(k);
+            //     console.log(ref.child(k));
+            //     await app.database().ref(`volunteers/${k}`).remove();
+            //     marker.setMap(null);
+            //   }
+            // });
+        }
+    });
 }
--- js/search-volunteers.js
+++ js/search-volunteers.js
@@ -5,13 +5,20 @@ const loadingSpinner = $('.nearby-volunteers-loading-spinner');
 
 //     <img class="nearby-volunteers-search-result__avatar" src="//images.weserv.nl/?url=unavatar.now.sh/${email}&w=125&h=125&mask=circle">
 
-const SearchResult = ({ name, distance, email, phone }) => sanitize `
+const SearchResult = ({ name, distance, email }) => sanitize `
   <div class="nearby-volunteers-search-result">
     <address class="nearby-volunteers-search-result__contact-info">
       <center><h3 class="nearby-volunteers-search-result__name">${name}</h3></center>
       <center><h4 class="nearby-volunteers-search-result__distance">${distance.toFixed(2)} miles away</h4></center>
       <center><h4 class="nearby-volunteers-search-result__email">${email}</h4></center>
-      <center><h4 class="nearby-volunteers-search-result__phone">${phone}</h4></center>
+    </address>
+  </div> 
+`;
+
+const Kumar = (string) => sanitize `
+  <div class="nearby-volunteers-search-result">
+    <address class="nearby-volunteers-search-result__contact-info">
+      <center><h3 class="nearby-volunteers-search-result__name">${string}</h3></center>
     </address>
   </div> 
 `;
@@ -21,7 +28,7 @@ const miToKm = mi => mi / 0.62137;
 
 const search = async section => {
     // remove all stale results
-    volunteersList.empty(); 
+    volunteersList.empty();
     searchResults.length = 0;
 
     loadingSpinner.show();
@@ -29,23 +36,34 @@ const search = async section => {
     const address = $(`${section} #address3`).val();
     const { lat, lng } = await toCoords(address);
     const center = [lat, lng];
-    const radius = miToKm(0.5); // geofire uses km
+    const radius = miToKm(2.5); // geofire uses km
 
     const geoQuery = geoFireVolunteers.query({ center, radius });
-
+    const bool = false;
     geoQuery.on('key_entered', async(geoKey, _, distance) => {
-      const key = geoKey.substring('geo'.length);
-      const snapshot = await refVolunteers.child(key).once('value');
-      const { name, email, phone } = snapshot.val();
-        
-      distance = kmToMi(distance);
+        const key = geoKey.substring('geo'.length);
+        const snapshot = await refVolunteers.child(key).once('value');
+        const { name, email, phone } = snapshot.val();
 
-      searchResults.push({ name, distance, email, phone });
-      searchResults.sort((a, b) => a.distance - b.distance);
-    
-      volunteersList.html(
-        searchResults.map(SearchResult).join(''));
+        distance = kmToMi(distance);
+
+        searchResults.push({ name, distance, email, phone });
+        searchResults.sort((a, b) => a.distance - b.distance);
+
+        volunteersList.html(
+            searchResults.map(SearchResult).join(''));
+        const key = geoKey.substring('geo'.length);
+        const snapshot = await refVolunteers.child(key).once('value');
+        const { name, email } = snapshot.val();
+        if (name != null) {
+            bool = true;
+        }
+        nearbyVolunteersList.prepend(SearchResult({ name, distance, email }));
     });
+
+    if (!bool) {
+        nearbyVolunteersList.prepend(Kumar("Contact Rishi Kumar, Email Campaign@Rishi2020.com"));
+    }
 
     // 'ready' means query is done
     geoQuery.on('ready', () => loadingSpinner.hide());
