b1fc43f114c1326091bb9eb1c70d3702ca1fd82a
rvignav <rvignav@gmail.com>
2020-03-26 12:36:51 -0700

    fixed enter function

--- js/form.js
+++ js/form.js
@@ -1,55 +1,53 @@
-const addToFirebase = async (ref, obj) => {
-  const { address } = obj;
-  const { lat, lng } = await toCoords(address);
-  const key = ref.push().key;
-  const geoFire = new geofire.GeoFire(ref)
-  await geoFire.set(`geo${key}`, [lat, lng]);
+const addToFirebase = async(ref, obj) => {
+    const { address } = obj;
+    const { lat, lng } = await toCoords(address);
+    const key = ref.push().key;
+    const geoFire = new geofire.GeoFire(ref)
+    await geoFire.set(`geo${key}`, [lat, lng]);
 
-  ref.child(key).set(obj);
+    ref.child(key).set(obj);
 }
 
-var submit = async function (section) {
-  var name = $(`${section} #name`).val();
-  var email = $(`${section} #email`).val();
-  var address = $(`${section} #address`).val();
-  var subject = $(`${section} #subject`).val();
-  var message = $(`${section} #message`).val();
+var submit = async function(section) {
+    var name = $(`${section} #name`).val();
+    var email = $(`${section} #email`).val();
+    var address = $(`${section} #address`).val();
+    var subject = $(`${section} #subject`).val();
+    var message = $(`${section} #message`).val();
 
-  await addToFirebase(refMarkers, { name, email, address, subject, message });
+    await addToFirebase(refMarkers, { name, email, address, subject, message });
 };
 
-var submit2 = async function (section) {
-  var name = $(`${section} #name2`).val();
-  var email = $(`${section} #email2`).val();
-  var address = $(`${section} #address2`).val();
-  var phone = $(`${section} #phone`).val();
-  
-  await addToFirebase(refVolunteers, { name, email, address, phone });
+var submit2 = async function(section) {
+    var name = $(`${section} #name2`).val();
+    var email = $(`${section} #email2`).val();
+    var address = $(`${section} #address2`).val();
+    var phone = $(`${section} #phone`).val();
+
+    await addToFirebase(refVolunteers, { name, email, address, phone });
 };
 
 $("#formbutton").click(async e => {
-  e.preventDefault();
-  console.log('IN SUBMIT');
-  await submit('#input');
-  location.reload();
+    e.preventDefault();
+    console.log('IN SUBMIT');
+    await submit('#input');
+    location.reload();
 });
 
 $("#formbutton2").click(async e => {
-  e.preventDefault();
-  console.log('IN SUBMIT');
-  await submit2('#input2');
-  location.reload();
+    e.preventDefault();
+    console.log('IN SUBMIT');
+    await submit2('#input2');
+    location.reload();
 });
 
 $("#searchForm").submit(e => {
-  e.preventDefault();
-  search('#map2');
+    e.preventDefault();
+    search('#map2');
 });
 
 $("#searchbutton").click(e => {
-  e.preventDefault();
-  console.log('IN SUBMIT');
-  // $('.nearby-volunteers-list').show();
-  // $('.nearby-volunteers-loading-spinner').show();
-  search('#map2');
+    e.preventDefault();
+    console.log('IN SUBMIT');
+    search('#map2');
 });
