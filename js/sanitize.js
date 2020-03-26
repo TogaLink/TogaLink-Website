c63ea124339c11cbaa6de679d5d453889bd52d25
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-03-25 17:21:03 -0700

    *gasp* you cant use es2020 without babel, can you?

--- js/sanitize.js
+++ js/sanitize.js
@@ -1,5 +1,5 @@
 // sanitize Firebase fields against possible XSS attacks
 const sanitize = (strings, ...values) => {
-  const dirty = strings.reduce((prev, next, i) => `${prev}${next}${values[i]} || ''}`, '');
-  return DomPurify.sanitize(dirty);
+  const dirty = strings.reduce((prev, next, i) => `${prev}${next}${values[i] ?? ''}`, '');
+  return DOMPurify.sanitize(dirty);
 }
