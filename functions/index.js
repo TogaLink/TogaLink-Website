ae6571f4d13ac828e359c4c66f17026f849fd2dd
rvignav <rvignav@gmail.com>
2020-03-26 20:48:57 -0700

    fix email

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ admin.initializeApp();
 
 let config = require('./env.json');
 if (Object.keys(functions.config()).length) {
-  config = functions.config();
+    config = functions.config();
 }
 
 // using Twilio SendGrid's v3 Node.js Library
@@ -16,18 +16,18 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(config.sendgrid.key);
 
 exports.onMarkerCreate = functions.database.ref('/markers/{postId}').onCreate((snapshot, _) => {
-  const { name, email, address, subject, message } = snapshot.val();
-  const msg = {
-    to: 'togalink2020@gmail.com', // TODO: change this to campaign@rishi2020.com
-    from: 'info@togalink.org',
-    replyTo: email,
-    subject: `[TogaLink] COVIDcare: ${name} needs your help!`,
-    text: 'Please view in html',
-    html: `${name} (${email}) has placed a marker at ${address}.
+    const { name, email, address, subject, message } = snapshot.val();
+    const msg = {
+        to: 'campaign@rishi2020.com',
+        from: 'info@togalink.org',
+        replyTo: email,
+        subject: `[TogaLink] COVIDcare: ${name} needs your help!`,
+        text: 'Please view in html',
+        html: `${name} (${email}) has placed a marker at ${address}.
     <br><br>
     <strong>Subject</strong>: ${subject}
     <br>
     <strong>Message</strong>: ${message}`,
-  };
-  return sgMail.send(msg);
+    };
+    return sgMail.send(msg);
 });
