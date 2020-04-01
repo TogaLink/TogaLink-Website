c397a4c246f862a83ca78689b2e159965f539eed
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-03-31 21:26:28 -0700

    stable

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,6 @@ const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   plugins: [
-    postcssPresetEnv({
-      autoprefixer: { grid: true }
-    })
+    postcssPresetEnv()
   ]
 }
