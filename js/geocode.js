ec88536465ede4b4bbf535eeb171d49b0e52d3c6
rvignav <rvignav@gmail.com>
2020-04-02 12:18:22 -0700

    fix geocoding

--- js/geocode.js
+++ js/geocode.js
@@ -1,14 +1,14 @@
 const toCoords = async address => {
-  const [response, err] = await handle(axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-    params: {
-      address,
-      key: 'AIzaSyCUmA1jvhKOYygqrQMVJi8IJmXuW496HGk'
+    const [response, err] = await handle(axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+            address,
+            key: 'AIzaSyCuZPmaKymva7t2qEloYezKPm2Nbqf3VLE'
+        }
+    }));
+    if (err ? .response ? .status === 400) { // address was empty
+        throw new Error("The address field cannot be empty");
+    } else if (response ? .data ? .results ? .length === 0) {
+        throw new Error("The specified address was not found");
     }
-  }));
-  if (err?.response?.status === 400) { // address was empty
-    throw new Error("The address field cannot be empty");
-  } else if (response?.data?.results?.length === 0) {
-    throw new Error("The specified address was not found");
-  }
-  return response.data.results[0].geometry.location;
+    return response.data.results[0].geometry.location;
 };
