7afdd5d0ff8948d464dec5cebd35c53d7a6626b9
rvignav <rvignav@gmail.com>
2020-04-02 19:24:59 -0700

    geocoding filtering

--- js/geocode.js
+++ js/geocode.js
@@ -4,22 +4,28 @@ let geocoder;
 $(document).ready(() => (geocoder = new google.maps.Geocoder()));
 
 const geocode = (address) =>
-  new Promise((accept, reject) => {
-    geocoder.geocode({ address }, (result, status) => {
-      if (status === 'OK') {
-        accept(result);
-      }
-      reject(status);
+    new Promise((accept, reject) => {
+        geocoder.geocode({
+            'address': address,
+            componentRestrictions: {
+                country: 'US',
+                administrativeArea: 'CA'
+            }
+        }, (result, status) => {
+            if (status === 'OK') {
+                accept(result);
+            }
+            reject(status);
+        });
     });
-  });
 
-const toCoords = async (address) => {
-  const [response, err] = await handle(geocode(address));
-  if (err === 'ZERO_RESULTS') {
-    // address was empty
-    throw new Error('The inputted address cannot be empty');
-  } else if (response?.length === 0) {
-    throw new Error('The specified address was not found');
-  }
-  return response[0].geometry.location;
+const toCoords = async(address) => {
+    const [response, err] = await handle(geocode(address));
+    if (err === 'ZERO_RESULTS') {
+        // address was empty
+        throw new Error('The inputted address cannot be empty');
+    } else if (response ? .length === 0) {
+        throw new Error('The specified address was not found');
+    }
+    return response[0].geometry.location;
 };
